290417ed1e439c719889eda0023254ae3cb7a3e9
Melissa Stone Rogers <melissastonerogers@gmail.com>
2025-02-24 00:50:57 -0600

    additional updates removing unneeded kafka/zookeeper items

--- utils/utils_config.py
+++ utils/utils_config.py
@@ -37,28 +37,6 @@ load_dotenv()
 # Getter Functions for .env Variables
 #####################################
 
-
-def get_zookeeper_address() -> str:
-    """Fetch ZOOKEEPER_ADDRESS from environment or use default."""
-    address = os.getenv("ZOOKEEPER_ADDRESS", "127.0.0.1:2181")
-    logger.info(f"ZOOKEEPER_ADDRESS: {address}")
-    return address
-
-
-def get_kafka_broker_address() -> str:
-    """Fetch KAFKA_BROKER_ADDRESS from environment or use default."""
-    address = os.getenv("KAFKA_BROKER_ADDRESS", "127.0.0.1:9092")
-    logger.info(f"KAFKA_BROKER_ADDRESS: {address}")
-    return address
-
-
-def get_kafka_topic() -> str:
-    """Fetch the correct Kafka topic from environment variables or use default."""
-    topic = os.getenv("KAFKA_TOPIC", "news_topic")  # Set "news_topic" as the default
-    logger.info(f"KAFKA_TOPIC: {topic}")
-    return topic
-
-
 def get_message_interval_seconds_as_int() -> int:
     """Fetch MESSAGE_INTERVAL_SECONDS from environment or use default."""
     try:
@@ -68,14 +46,6 @@ def get_message_interval_seconds_as_int() -> int:
     except ValueError:
         logger.error("Invalid value for MESSAGE_INTERVAL_SECONDS; using default (5).")
         return 5
-
-
-def get_kafka_consumer_group_id() -> str:
-    """Fetch NEWS_CONSUMER_GROUP_ID from environment or use default."""
-    group_id = os.getenv("NEWS_CONSUMER_GROUP_ID", "news_group")
-    logger.info(f"NEWS_CONSUMER_GROUP_ID: {group_id}")
-    return group_id
-
 
 def get_base_data_path() -> pathlib.Path:
     """Fetch BASE_DATA_DIR from environment or use default."""
@@ -110,7 +80,6 @@ def get_postgres_port() -> int:
         return 5432
 
 
-
 def get_postgres_db() -> str:
     """Fetch POSTGRES_DB from environment or use default."""
     db = os.getenv("POSTGRES_DB", "postgres_buzz_database")
@@ -140,11 +109,7 @@ if __name__ == "__main__":
     # Test the configuration functions
     logger.info("Testing configuration.")
     try:
-        get_zookeeper_address()
-        get_kafka_broker_address()
-        get_kafka_topic()
         get_message_interval_seconds_as_int()
-        get_kafka_consumer_group_id()
         get_base_data_path()
         get_database_type()
         get_postgres_host()
